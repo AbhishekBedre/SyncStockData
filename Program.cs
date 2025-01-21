a00dc653231b2f614073d6c26c1bd53ce0db3281
AbhishekBedre <abhishekbedre@gmail.com>
2025-01-21 19:25:52 +0530

    issue fixed

--- Program.cs
+++ Program.cs
@@ -171,7 +171,7 @@ class Program
                             trigger.ForJob(indexlastSession).WithCronSchedule("0 0-30/5 15 ? * MON-FRI"); // From 3:00 PM to 3:30 PM, Monday to Friday
                         });
 
-                    var indexFinalCall = JobKey.Create("indexFinalCall ");
+                    var indexFinalCall = JobKey.Create("indexFinalCall");
 
                     q.AddJob<BroderMarketsUpdateJob>(indexFinalCall)
                         .AddTrigger(trigger =>
